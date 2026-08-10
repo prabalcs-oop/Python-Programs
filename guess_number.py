3b8a9e581ab9286c284fd1ddd964b5c6f169d99b
prabalcs-oop <chaudharyprabal06@gmail.com>
2026-08-10 21:45:41 +0530

    Implement input validation for guessing game
    Added error handling for non-integer inputs.

--- guess_number.py
+++ guess_number.py
@@ -1,14 +1,22 @@
+
+
+# ...existing code...
 import random
 
 secret = random.randint(1, 100)
 
-guess = int(input("Guess a number between 1 and 100: "))
+while True:
+    try:
+        guess = int(input("Guess a number between 1 and 100: "))
+    except ValueError:
+        print("Please enter a valid integer.")
+        continue
 
-if guess == secret:
-    print("You guessed it!")
-elif guess < secret:
-    print("Too low.")
-else:
-    print("Too high.")
+    if guess == secret:
+        print("You guessed it!")
+        break
+    elif guess < secret:
+        print("Too low.")
+    else:
+        print("Too high.")
 # ...existing code...
-
