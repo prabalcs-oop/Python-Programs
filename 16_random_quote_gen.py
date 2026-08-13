f7c7c3f1637bfe83b33c0df7358850ce7d2a9b99
prabalcs-oop <chaudharyprabal06@gmail.com>
2026-08-13 22:05:30 +0530

    16_random_quote_gen.py

---
+++ 16_random_quote_gen.py
@@ -0,0 +1,14 @@
+import random
+
+quotes = [
+    "Believe in yourself:",
+    "Never give up:",
+    "Stay positive:",
+    "success comes with great work :",
+    "Keep learning and growing:",
+    "Dream big and work hard:",
+]
+quote = random.choice(quotes)
+
+print("Random quote:")
+print(quote)
